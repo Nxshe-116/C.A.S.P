3e0442741fd9bf42d15cb1ee7c0d39efd8a20a6f
Nashe Chagumaira <77992898+Nxshe-116@users.noreply.github.com>
2025-03-17 03:36:58 +1100

    removed dependancies

--- lib/screens/dashboard/components/my_fields.dart
+++ lib/screens/dashboard/components/my_fields.dart
@@ -5,8 +5,7 @@ import 'package:admin/responsive.dart';
 import 'package:admin/services/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
+ 
 
 import '../../../constants.dart';
 import 'file_info_card.dart';
